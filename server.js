eed2fa2bc5097a94a8279cc1839cdd4d1b12996f
Austin Miedema <r.austin.mie@gmail.com>
2019-07-19 13:18:47 -0700

    server running

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require("express");
-const path = require("path");
+try {
+	const express = require("express");
+	const path = require("path");
+	const helmet = require("helmet");
 
-const app = express();
+	const app = express();
+	app.use(helmet());
 
-app.use("/", express.static(path.join(__dirname, ".")));
+	app.use("/", express.static(path.join(__dirname, ".")));
+	app.listen(process.env.PORT || 8080, () => {
+		console.log("server running");
+	});
+	console.log("finish");
+} catch (e) {
+	console.error(e);
+}
