fdd7a29366e9df726a6c52d86ee1e6ef72e5f587
Austin Miedema <r.austin.mie@gmail.com>
2019-07-19 19:25:19 -0700

    update package.json

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 try {
 	const express = require("express");
 	const path = require("path");
-	const helmet = require("helmet");
 
 	const app = express();
-	app.use(helmet());
 
 	app.use("/", express.static(path.join(__dirname, ".")));
 	app.listen(process.env.PORT || 8080, () => {
